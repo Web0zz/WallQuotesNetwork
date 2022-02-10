8508edf54d1e561e1b86fe345050a1f2f3abf579
Web0zz <furkan.ozen4@ogr.sakarya.edu.tr>
2022-02-11 00:04:38 +0300

    Timber added to application

--- app/src/main/java/com/web0zz/wallquotes/MainApplication.kt
+++ app/src/main/java/com/web0zz/wallquotes/MainApplication.kt
@@ -2,6 +2,24 @@ package com.web0zz.wallquotes
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initLogger()
+    }
+
+    private fun initLogger() {
+        if(BuildConfig.DEBUG) {
+            Timber.plant(
+                object: Timber.DebugTree() {
+                    override fun createStackElementTag(element: StackTraceElement): String {
+                        return "(${element.fileName}:${element.lineNumber}:${Thread.currentThread().name})#${element.methodName}"
+                    }
+                }
+            )
+        }
+    }
+}
